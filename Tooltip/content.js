4caea9db329e0dd5a5218ab9401f3ae0d9296b25
Petros Devrikis <petrosdevri@protonmail.com>
2023-01-20 01:25:34 +0200

    Set up the main files

---
+++ Tooltip/content.js
@@ -0,0 +1,134 @@
+/*const svgs = `
+        <svg class="tooltip-icon">
+            <path d="M19.074 21.117c-1.244 0-2.432-.37-3.532-1.096a7.792 7.792 0 0 1-.703-.52c-.77.21-1.57.32-2.38.32-4.67 0-8.46-3.5-8.46-7.8C4 7.7 7.79 4.2 12.46 4.2c4.662 0 8.457 3.5 8.457 7.803 0 2.058-.85 3.984-2.403 5.448.023.17.06.35.118.55.192.69.537 1.38 1.026 2.04.15.21.172.48.058.7a.686.686 0 0 1-.613.38h-.03z" fill-rule="evenodd"></path>
+        </svg>
+        <svg class="tooltip-icon">
+           <path d="M19.074 21.117c-1.244 0-2.432-.37-3.532-1.096a7.792 7.792 0 0 1-.703-.52c-.77.21-1.57.32-2.38.32-4.67 0-8.46-3.5-8.46-7.8C4 7.7 7.79 4.2 12.46 4.2c4.662 0 8.457 3.5 8.457 7.803 0 2.058-.85 3.984-2.403 5.448.023.17.06.35.118.55.192.69.537 1.38 1.026 2.04.15.21.172.48.058.7a.686.686 0 0 1-.613.38h-.03z" fill-rule="evenodd"></path>
+        </svg>
+        <svg class="tooltip-icon">
+            <path d="M19.074 21.117c-1.244 0-2.432-.37-3.532-1.096a7.792 7.792 0 0 1-.703-.52c-.77.21-1.57.32-2.38.32-4.67 0-8.46-3.5-8.46-7.8C4 7.7 7.79 4.2 12.46 4.2c4.662 0 8.457 3.5 8.457 7.803 0 2.058-.85 3.984-2.403 5.448.023.17.06.35.118.55.192.69.537 1.38 1.026 2.04.15.21.172.48.058.7a.686.686 0 0 1-.613.38h-.03z" fill-rule="evenodd"></path>
+        </svg>
+        <svg class="tooltip-icon">
+            <path d="M19.074 21.117c-1.244 0-2.432-.37-3.532-1.096a7.792 7.792 0 0 1-.703-.52c-.77.21-1.57.32-2.38.32-4.67 0-8.46-3.5-8.46-7.8C4 7.7 7.79 4.2 12.46 4.2c4.662 0 8.457 3.5 8.457 7.803 0 2.058-.85 3.984-2.403 5.448.023.17.06.35.118.55.192.69.537 1.38 1.026 2.04.15.21.172.48.058.7a.686.686 0 0 1-.613.38h-.03z" fill-rule="evenodd"></path>
+        </svg>
+`;
+
+const toolTip = document.createElement("div");
+toolTip.classList.add('tooltip');
+toolTip.innerHTML = svgs;
+
+const toolTipTail = document.createEleemnt("div");
+toolTipTail.classList.add("tooltip-tail");
+
+document.onmousedown = () => {
+    const selectedText = window.getSelection();
+    const anchorNode = selectedText.anchorNode;
+    const focusNode = selectedText.focusNode;
+
+    if(anchorNode != focusNode) {
+        return;
+    }
+
+    const rangeRect = selectedText.getRangeAt(0).getClientRects()[0];
+
+    document.body.appendChild(toolTip);
+    document.body.appendChild(toolTipTail);
+
+    const toolTipWidth = toolTip.offsetWidth;
+    const toolTipHeight = toolTip.offsetHeight;
+    const toolTipTailWidth = toolTipTail.offsetWidth;
+    const toolTipTailHeight = toolTipTail.offsetHeight;
+
+    const y = rangeRect.y;
+    const middleX = rangeRect.x + (rangeRect.width/2);
+    toolTip.style.left = `${middleX - toolTipWidth/2 }px`;
+    toolTip.style.top = `${y - toolTipHeight - toolTipTailHeight/2}px`;
+
+    toolTipTail.style.left = `${middleX - toolTipTailWidth/2 }px`;
+    toolTipTail.style.top = `${y - toolTipTailHeight/2}px`;
+}*/
+
+const svgs = `
+    <svg class="tooltip__icon">
+      <path d="M19.074 21.117c-1.244 0-2.432-.37-3.532-1.096a7.792 7.792 0 0 1-.703-.52c-.77.21-1.57.32-2.38.32-4.67 0-8.46-3.5-8.46-7.8C4 7.7 7.79 4.2 12.46 4.2c4.662 0 8.457 3.5 8.457 7.803 0 2.058-.85 3.984-2.403 5.448.023.17.06.35.118.55.192.69.537 1.38 1.026 2.04.15.21.172.48.058.7a.686.686 0 0 1-.613.38h-.03z" fill-rule="evenodd"></path>
+    </svg>
+    <svg class="tooltip__icon">
+      <path d="M19.074 21.117c-1.244 0-2.432-.37-3.532-1.096a7.792 7.792 0 0 1-.703-.52c-.77.21-1.57.32-2.38.32-4.67 0-8.46-3.5-8.46-7.8C4 7.7 7.79 4.2 12.46 4.2c4.662 0 8.457 3.5 8.457 7.803 0 2.058-.85 3.984-2.403 5.448.023.17.06.35.118.55.192.69.537 1.38 1.026 2.04.15.21.172.48.058.7a.686.686 0 0 1-.613.38h-.03z" fill-rule="evenodd"></path>
+    </svg>
+    <svg class="tooltip__icon">
+      <path d="M19.074 21.117c-1.244 0-2.432-.37-3.532-1.096a7.792 7.792 0 0 1-.703-.52c-.77.21-1.57.32-2.38.32-4.67 0-8.46-3.5-8.46-7.8C4 7.7 7.79 4.2 12.46 4.2c4.662 0 8.457 3.5 8.457 7.803 0 2.058-.85 3.984-2.403 5.448.023.17.06.35.118.55.192.69.537 1.38 1.026 2.04.15.21.172.48.058.7a.686.686 0 0 1-.613.38h-.03z" fill-rule="evenodd"></path>
+    </svg>
+    <svg class="tooltip__icon">
+      <path d="M19.074 21.117c-1.244 0-2.432-.37-3.532-1.096a7.792 7.792 0 0 1-.703-.52c-.77.21-1.57.32-2.38.32-4.67 0-8.46-3.5-8.46-7.8C4 7.7 7.79 4.2 12.46 4.2c4.662 0 8.457 3.5 8.457 7.803 0 2.058-.85 3.984-2.403 5.448.023.17.06.35.118.55.192.69.537 1.38 1.026 2.04.15.21.172.48.058.7a.686.686 0 0 1-.613.38h-.03z" fill-rule="evenodd"></path>
+    </svg>
+`;
+
+const toolTip = document.createElement("div");
+toolTip.classList.add("tooltip");
+toolTip.innerHTML = svgs;
+
+const toolTipTail = document.createElement("div");
+toolTipTail.classList.add("tooltip-tail");
+
+const articleElement = document.getElementsByClassName("article")[0];
+
+function removeTooltip() {
+    if (document.body.contains(toolTip)) {
+      document.body.removeChild(toolTip);
+      document.body.removeChild(toolTipTail);
+    }
+  }
+  
+  let selectionQueued = false;
+  
+  function displayTooltip() {
+    const selection = document.getSelection();
+    const anchorNode = selection.anchorNode;
+    const focusNode = selection.focusNode;
+  
+    document.body.appendChild(toolTip);
+    document.body.appendChild(toolTipTail);
+  
+    const toolTipWidth = toolTip.offsetWidth;
+    const toolTipHeight = toolTip.offsetHeight;
+    const toolTipTailWidth = toolTipTail.offsetWidth;
+    const toolTipTailHeight = toolTipTail.offsetHeight;
+  
+    const rangeRects = selection.getRangeAt(0).getClientRects();
+  
+    const parentElement = selection.anchorNode.parentElement;
+    const y = rangeRects[0].y;
+    const x = rangeRects.length > 1 ?
+      parentElement.offsetLeft + parentElement.offsetWidth/2 :
+      rangeRects[0].x + rangeRects[0].width/2;
+  
+    toolTip.style.top = `${y - toolTipHeight - toolTipTailHeight/2}px`;
+    toolTip.style.left = `${x - toolTipWidth/2}px`;
+  
+    toolTipTail.style.top = `${y - toolTipTailHeight/2}px`;
+    toolTipTail.style.left = `${x - toolTipTailWidth/2}px`;
+  
+  }
+  
+  document.onmouseup = () => {
+    if (selectionQueued) {
+      displayTooltip();
+    }
+    selectionQueued = false;
+  }
+  
+  document.addEventListener("selectionchange", function(event) {
+    const selection = document.getSelection();
+    if (selection.type !== "Range") {
+      removeTooltip();
+      return;
+    }
+  
+    if (selection.anchorNode != selection.focusNode) {
+      // Cross-paragraph selection
+      selectionQueued = false;
+      return;
+    }
+    
+    selectionQueued = true;
+  });
